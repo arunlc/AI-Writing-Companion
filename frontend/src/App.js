3b235319bb09696608270ef5572b8e95b8983c3e
arunlc <arun.k@spinircle.com>
2025-04-24 18:44:37 +0530

    Create App.js
    Create a basic frontend App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="bg-indigo-700 text-white p-4">
+        <div className="container mx-auto">
+          <h1 className="text-2xl font-bold">Learner Circle Writing Companion</h1>
+        </div>
+      </header>
+      <main className="container mx-auto py-6 px-4">
+        <p>Welcome to the Writing Companion app!</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
