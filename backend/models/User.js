f003b9c1bb99dfd6a568c1e67a4b0807d095118c
arunlc <arun.k@spinircle.com>
2025-04-24 18:42:26 +0530

    Create User.js
    For the backend models directory

---
+++ backend/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  grade: String,
+  coins: {
+    type: Number,
+    default: 0
+  },
+  level: {
+    type: Number,
+    default: 1
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', UserSchema);
