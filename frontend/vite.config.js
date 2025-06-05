7932191eae9ccd8242ccfc135ab40c6f04435b4f
arunlc <arun.k@spinircle.com>
2025-06-05 23:14:05 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: './postcss.config.js',
+  },
   server: {
     port: 3000,
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://ai-writing-companion-api.onrender.com',
         changeOrigin: true,
         secure: false,
       }
